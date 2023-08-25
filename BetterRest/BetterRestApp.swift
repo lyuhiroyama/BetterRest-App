3a6d47587b6d3eb82e1abcd5fd56a2b9b0ec9fdb
Lyu.Hiroyama <lyuhiroyama@gmail.com>
2023-08-26 00:03:02 +0900

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Lyu Hiroyama on 2023/08/26.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
